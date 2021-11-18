d93ffc7a3662351dbc7370789ff5ec7fe80490af
ktkr93 <ktkr93@gmail.com>
2021-11-18 16:35:21 +0900

    modify google tag managerのタグを記述

--- Created/Suzuki-Fishing/wp-content/themes/mytheme/header.php
+++ Created/Suzuki-Fishing/wp-content/themes/mytheme/header.php
@@ -1,3 +1,22 @@
+<!-- Google Tag Manager -->
+<script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-W76H9PT');
+</script>
+<!-- End Google Tag Manager -->
+
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
--- Created/Suzuki-Fishing/wp-content/themes/mytheme/page-archive.php
+++ Created/Suzuki-Fishing/wp-content/themes/mytheme/page-archive.php
@@ -12,6 +12,9 @@
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W76H9PT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <header>
         <?php get_template_part('includes/header') ?>
     </header>
